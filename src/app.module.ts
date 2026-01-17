73b76121e391d93d61c34a008cb54c7b34aff26a
salmahmed6 <sa1963645@gmail.com>
2026-01-17 22:51:16 +0200

    feat: Initialize core application structure with user authentication and property entities.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, UserModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule { }
