7f2298a70fdd6fd3af823b59ffb5742477f0877f
salmahmed6 <sa1963645@gmail.com>
2026-01-17 22:52:02 +0200

    feat: set up TypeORM entities for property and user management, database configurations, and authentication service with E2E tests.

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -1,7 +1,7 @@
 import request from 'supertest';
 import { Test } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import { AppModule } from '../app.module';
+import { AppModule } from '../src/app.module';
 
 describe('Auth E2E', () => {
     let app: INestApplication;
