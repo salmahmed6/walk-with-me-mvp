50089d44386e2ae9b8dfa8de8cd619f2f974554c
Salma <sa1963645@gmail.com>
2026-01-26 23:14:58 +0200

    refactor: enhance UpdateUserDto with additional validation for bio and avatarUrl fields

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,4 +1,22 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsOptional, IsString, IsUrl, MaxLength } from 'class-validator';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+    @IsOptional()
+    @IsString()
+    firstName?: string;
+
+    @IsOptional()
+    @IsString()
+    lastName?: string;
+
+    @IsOptional()
+    @IsString()
+    @MaxLength(200)
+    bio?: string;
+
+    @IsOptional()
+    @IsUrl()
+    avatarUrl?: string;
+}
